db7e5f8b875aae9a53b5f8710ca0a4f7d929f3da
LunaTheSnake <109224823+LunaTheSnake@users.noreply.github.com>
2023-02-05 21:04:28 -0400

    Add files via upload

---
+++ project100.py
@@ -0,0 +1,9 @@
+class Atm:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber=cardnumber
+        self.pin=pin
+
+    def self check_balance(self)
+        print("Your balance is 50000")
+
+    new_user=ATM(Card_number,pin_number)
